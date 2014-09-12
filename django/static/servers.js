b5215804f76a366f5c25aa2a2da3a84445b7ec92
leal.yang <332802744@qq.com>
2014-09-12 16:17:50 +0800

    servers

--- django/static/servers.js
+++ django/static/servers.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+
+    $('#svr-add-idc').combobox({
+        onChange:function(newValue, oldValue) {
+            $('#svr-add-cab').combobox({
+                onBeforeLoad:function(param) {
+                param.idcid=newValue
+            },
+            'url' : '/resource/get-cabs/'}
+            )}
+    })
+
     $('#svr-display-table').datagrid({
         url:'xxx.json',
         title:'服务器列表',
@@ -16,20 +27,22 @@ $(document).ready(function(){
         striped:true,
         columns:[[
 //            {field:'svr-id':title:'ID',with 5},
-//            {field:'idc-id', title:'IDC-ID'},
-//            {field:'user-id', title:'User-ID'},
-//            {field:'svr-usable-id', title:'usable'},
+//            {field:'cab-id', title:'CAB-ID'},
+//            {field:'admin-id', title:'User-ID'},
+//            {field:'svr-used-type', title:'usable'},
+//            {field:'idc-id', title:'iid'},
+//            {field:'father-id', title:'fid'},
             {field:'svr-name', title:'编号', width:40},
             {field:'idc-name', title:'所在机房', width:40},
             {field:'cab-name', title:'所属机柜', width:40},
             {field:'svr-size', title:'规格', width:20},
-            {field:'svr-parts', title:'配置', width:40},
+            {field:'svr-parts', title:'配置', width:60},
+            {field:'svr-os', title:'操作系统', width:40},
             {field:'storage-date', title:'入库日期', width:40},
             {field:'end-date', title:'到期日期', width:40},
-            {field:'father-server', title:'父级', width:40},
-            {field:'pro-name', title:'分配IP', width:60},
-            {field:'svr-usable', title:'可用', width:20},
-            {field:'op-name', title:'负责人', width:40}
+            {field:'svr-father', title:'父级', width:40},
+            {field:'svr-usable', title:'类型', width:20},
+            {field:'admin-name', title:'负责人', width:40}
         ]],
         onSelect:function(rowIndex, rowData) {
             $('#btn-update').linkbutton('enable')
@@ -51,7 +64,7 @@ $(document).ready(function(){
                 father_tip()
                 $('#svr-add-new').dialog('open'),
                     $('#svr-admin').combobox({'url':'/resource/get-users/'}),
-                    $('#svr-idc').combobox({'url':'/resource/get-idcs/'})
+                    $('#svr-add-idc').combobox({'url':'/resource/get-idcs/'})
             }
         },{
             id:'btn-update',
@@ -74,14 +87,14 @@ $(document).ready(function(){
                     if (r){
                         var val = $('#svr-display-table').datagrid('getSelected')
                         if (val) {
-                            var cid=val['svr-id']
-                            $.post('/resource/del-cab/',
-                                {'cid':cid},
+                            var sid=val['svr-id']
+                            $.post('/resource/del-server/',
+                                {'sid':sid},
                                 function(data, status) {
                                     if(status == 'success' && data['status'] == 'success') {
                                         alert("删除成功!")
                                         $('#svr-display-table').datagrid('reload')
-                                    } else {alert("删除失败，请重试!")}
+                                    } else {alert(data['data'])}
                                 })
                         }
                     }
@@ -91,7 +104,7 @@ $(document).ready(function(){
     })
     var p = $('#svr-display-table').datagrid('getPager');
     $(p).pagination({
-        pageSize:30,
+        pageSize:10,
         pageList:[10,30,50,100],
         beforePageText:'第',
         afterPageText:'页 共 {pages} 页',
@@ -100,40 +113,86 @@ $(document).ready(function(){
 
 })
 
-function cb_add_server_commit() {
-    $.post('/resource/add-cab/',
+function add_server_commit() {
+    $.post('/resource/add-server/',
         $('#svr-add-form').serialize(),
         function(data, status) {
-            if (status == 'success' && data['status']) {
+            if (status == 'success' && data['status'] == 'success') {
                 alert("添加成功!")
-                parent.location.reload()
+                $('#svr-add-new').dialog('close')
+                $('#svr-display-table').datagrid('reload')
             } else {alert("添加失败，请重试!")}
         })
 }
 
-function cb_add_server_cancel() {
+function add_server_cancel() {
     $('#svr-add-new').dialog('close')
 }
 
 function open_update_window() {
     var val = $('#svr-display-table').datagrid('getSelected')
     if (val) {
-        var uid=val['user-id']
-        var iid=val['idc-id']
-        var usable=val['svr-usable']
+        var usable = val['svr-used-type']
+        var x =[]
+        var key = ['测试','生产','不可用']
+        var s
+        for (i = 0; i < 3; i++) {
+            if (usable == i) {
+                s = {id:i,name:key[i],selected:true}
+            } else {
+                s = {id:i,name:key[i]}
+            }
+            x[i] = s
+        }
+
         $('#svr-id').attr('value', val['svr-id'])
         $('#svr-name').attr('value',val['svr-name'])
         $('#svr-size').attr('value',val['svr-size'])
+        $('#svr-update-os').attr('value',val['svr-os'])
         $('#svr-end-date').datebox('setValue',val['end-date'])
-        $('#svr-update-usable').combobox({'url':'resource/usable/?ub=' + usable + '&cid=' + val['svr-id'], 'method':'get'})
-        $('#svr-update-admin').combobox({'url':'/resource/get-users/?uid=' + uid, 'method':'get'})
-        $('#svr-update-idc').combobox({'url':'/resource/get-idcs/?idcid=' + iid, 'method':'get'})
+        $('#svr-update-parts').attr('value', val['svr-parts'])
+        $('#svr-update-idc').combobox({
+            onBeforeLoad:function(param) {
+                param.idcid = val['idc-id']
+            },
+            'url' : '/resource/get-idcs/',
+            onChange:function(newValue, oldValue) {
+                $('#svr-update-cab').combobox({
+                    onBeforeLoad:function(param) {
+                        param.idcid = $('#svr-update-idc').combobox('getValue')
+                        param.cabid = val['cab-id']
+                    },
+                    url:'/resource/get-cabs/',
+                    onChange:function(newValue, oldValue) {
+                        $('#svr-update-father').combobox({
+                            onBeforeLoad:function(param) {
+                                param.fid = val['father-id']
+                                param.cab = $('#svr-update-cab').combobox('getValue')
+                            },
+                            url:'/resource/get-servers/'
+                        })
+                    }
+                })
+            }
+        })
+        $('#svr-update-admin').combobox({
+            onBeforeLoad:function(param) {
+                param.uid = val['admin-id']
+            },
+            url:'/resource/get-users/'
+        })
+
+        $('#svr-update-usable').combobox({
+            valueField:'id',
+            textField:'name',
+            data:x
+        })
     }
     $('#svr-update-div').dialog('open')
 }
 
-function cb_update_commit() {
-    $.post('/resource/update-cab/',
+function svr_update_commit() {
+    $.post('/resource/update-server/',
         $('#svr-update-form').serialize(),
         function(data, status) {
             if (status == 'success' && data['status'] == 'success') {alert('修改成功')}
@@ -142,7 +201,7 @@ function cb_update_commit() {
         })
 }
 
-function cb_update_cancel() {
+function svr_update_cancel() {
     $('#svr-update-div').dialog('close')
 }
 
@@ -150,7 +209,14 @@ function check_father_selected() {
     if (!$('#svr-none-father').is(":checked")) {
         $('#svr-add-father').combobox('disable')
     } else {
-        $('#svr-add-father').combobox('enable')
+        var cab = $('#svr-add-cab').combobox('getValue')
+        $('#svr-add-father').combobox({
+            url:'/resource/get-servers/',
+            onBeforeLoad:function(param) {
+                param.cab = cab;
+            },
+            disabled:false
+        })
     }
 }
 
@@ -211,6 +277,11 @@ function father_tip() {
             })
         }
     })
+}
+
+function get_cab_base_on_idc() {
+    var iid = $('#svr-add-idc').combobox().getValue()
+    alert(iid)
 }
 
 
